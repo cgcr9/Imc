d74d030de215b065b51e28b74f6ca86eb3986820
Cristina Che Requena <15470137@itcampeche.edu.mx>
2018-10-16 16:50:06 -0500

    Imc: Primer versión funcional del proyecto. Incluye manejo de eventos.

--- app/src/main/java/com/cristinache/android/imc/MainActivity.java
+++ app/src/main/java/com/cristinache/android/imc/MainActivity.java
@@ -2,12 +2,51 @@ package com.cristinache.android.imc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mCampoPeso;
+    private EditText mCampoEstatura;
+    private Button mBotonCalcular;
+    private Button mBotonLimpiar;
+    private TextView mEtiquetaImc;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mCampoPeso = (EditText) findViewById(R.id.campo_peso);
+        mCampoEstatura = (EditText) findViewById(R.id.campo_estatura);
+        mBotonCalcular = (Button) findViewById(R.id.boton_calcular);
+        mBotonLimpiar = (Button) findViewById(R.id.boton_limpiar);
+        mEtiquetaImc = (TextView) findViewById(R.id.etiqueta_imc);
+
+        mBotonCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String s = mCampoPeso.getText().toString();
+                double peso = Double.parseDouble(s);
+                s = mCampoEstatura.getText().toString();
+                double estatura = Double.parseDouble(s);
+                double imc = peso / Math.pow(estatura, 2);
+                s = String.format("%2.2f", imc);
+                mEtiquetaImc.setText(s);
+            }
+        });
+
+        mBotonLimpiar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCampoPeso.setText("");
+                mCampoEstatura.setText("");
+                mEtiquetaImc.setText("0.0");
+            }
+        });
     }
 }
